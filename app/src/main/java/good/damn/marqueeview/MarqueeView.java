3173ee3dbbaa9057f7e092bc4f8e9097b0f6bb69
GoodDamn <76108467+GoodDamn@users.noreply.github.com>
2023-06-26 11:56:32 +0300

    Stick and his bounds

--- app/src/main/java/good/damn/marqueeview/MarqueeView.java
+++ app/src/main/java/good/damn/marqueeview/MarqueeView.java
@@ -3,7 +3,6 @@ package good.damn.marqueeview;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
-import android.graphics.Point;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -18,17 +17,20 @@ public class MarqueeView extends View implements View.OnTouchListener {
     private final Paint mPaint = new Paint();
     private final Paint mPaintInteract = new Paint();
     private final Paint mPaintDebug = new Paint();
+    private final Paint mPaintStick = new Paint();
 
-    private float mStartX = 0;
-    private float mStartY = 0;
-
-    private float mEndX = 0;
-    private float mEndY = 0;
+    private float mDeltaX = 0;
+    private float mDeltaY = 0;
 
     private float mMarStartX = 0;
     private float mMarStartY = 0;
     private float mMarEndX = 1;
     private float mMarEndY = 0;
+
+    private float mStickX = 0;
+    private float mStickY = 0;
+
+    private float mAngle = 0;
 
     private float mLenAngled = 5;
 
@@ -46,6 +48,8 @@ public class MarqueeView extends View implements View.OnTouchListener {
         mPaintInteract.setStrokeCap(Paint.Cap.ROUND);
 
         mPaintDebug.setColor(0xffffff00);
+
+        mPaintStick.setColor(0xff00ff59);
 
         setOnTouchListener(this);
     }
@@ -92,9 +96,9 @@ public class MarqueeView extends View implements View.OnTouchListener {
                 mMarEndY+mLenAngled,
                 mPaintDebug);
 
+        canvas.drawCircle(mStickX,mStickY, mPaintInteract.getStrokeWidth(), mPaintStick);
 
-
-        canvas.drawLine(mStartX, mStartY, mEndX, mEndY, mPaintInteract);
+        //canvas.drawLine(mStartX, mStartY, mEndX, mEndY, mPaintInteract);
     }
 
     @Override
@@ -107,41 +111,56 @@ public class MarqueeView extends View implements View.OnTouchListener {
         mMarEndY = getHeight() * 0.85f;
 
         // Calculate angle
-        float angle = (mMarEndX - mMarStartX) / (mMarEndY - mMarStartY);
-        mLenAngled = mLenBound * angle;
+        mAngle = (mMarEndX - mMarStartX) / (mMarEndY - mMarStartY);
+        mLenAngled = mLenBound * mAngle;
+
+        mStickX = mMarStartX;
+        mStickY = mMarStartY;
     }
 
     @Override
     public boolean onTouch(View view, MotionEvent motionEvent) {
 
-        Log.d(TAG, "onTouch: ACTION: " + motionEvent.getAction());
+        float x = motionEvent.getX();
+        float y = motionEvent.getY();
+
+        Log.d(TAG, "onTouch: X: " + x + " Y: " + y);
 
         switch (motionEvent.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                mStartX = motionEvent.getX();
-                mStartY = motionEvent.getY();
+                //mStartX = motionEvent.getX();
+                //mStartY = motionEvent.getY();
 
+                Log.d(TAG, "onTouch: STICK_X: " + mStickX + " STICK_Y: " + mStickY);
 
-                Log.d(TAG,"onTouch: TOP_LEFT: X: "+ (mMarStartX-mLenAngled) + " Y:" + (mMarStartY+mLenAngled));
-                Log.d(TAG,"onTouch: TOP_RIGHT: X: " + (mMarStartX+mLenAngled) + " Y: " + (mMarStartY-mLenAngled));
-                Log.d(TAG,"onTouch: BOTTOM_LEFT: X:" + (mMarEndX-mLenAngled) + " Y:" + (mMarEndY+mLenAngled));
-                Log.d(TAG,"onTouch: BOTTOM_RIGHT: X: " + (mMarEndX+mLenAngled) + " Y: " + (mMarEndY-mLenAngled));
-
-                Log.d(TAG, "onTouch: TOUCH_POS: X: " + mStartX + " Y:" + mStartY);
-
-                // Check in-bound touch
-                if (mMarStartX-mLenAngled < mStartX && mStartX < mMarStartX+mLenAngled) {
-
-                    Log.d(TAG, "onTouch: BOUNDED");
+                if (mStickX-20 < x && x < mStickX+20 &&
+                    mStickY-20 < y && y < mStickY+20) {
+                    Log.d(TAG, "onTouch: INSIDE_STICK_BOUND");
                 } else {
-                    Log.d(TAG, "onTouch: NOT_IN_BOUNDED");
                     return false;
                 }
 
+
+                mDeltaX = motionEvent.getX();
+                mDeltaY = motionEvent.getY();
+
                 break;
             case MotionEvent.ACTION_MOVE:
-                mEndX = motionEvent.getX();
-                mEndY = motionEvent.getY();
+                //mEndX = motionEvent.getX();
+                //mEndY = motionEvent.getY();
+
+                Log.d(TAG, "onTouch: DELTA_X: " + mDeltaX + " DELTA_Y: " + mDeltaY);
+
+                if (Math.abs(x-mDeltaX) < 12 && Math.abs(y-mDeltaY) < 12) {
+                    mStickX = x;
+                    mStickY = y;
+                } else {
+                    return false;
+                }
+
+                mDeltaX = x;
+                mDeltaY = y;
+
                 invalidate();
                 break;
         }
