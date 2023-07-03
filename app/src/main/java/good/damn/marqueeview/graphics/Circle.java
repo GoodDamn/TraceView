2cd2999a4fec341545dd6dff3f446b2266c9b970
GoodDamn <76108467+GoodDamn@users.noreply.github.com>
2023-07-03 12:24:55 +0300

    Circle foreground marquee (pt.1)

--- app/src/main/java/good/damn/marqueeview/graphics/Circle.java
+++ app/src/main/java/good/damn/marqueeview/graphics/Circle.java
@@ -12,6 +12,10 @@ public class Circle extends Entity {
     private static final String TAG = "Circle";
 
     private float mRadius;
+    private float mAngle = 0;
+
+    private float mDebugStickX = 0;
+    private float mDebugStickY = 0;
 
     public Circle() {
         super();
@@ -31,24 +35,52 @@ public class Circle extends Entity {
                 mMarStartY-mRadius,
                 mMarStartX+mRadius,
                 mMarStartY+mRadius,
-                180,360*mProgress,
+                0,360*mProgress,
                 false, mPaintForeground);
         canvas.drawCircle(mStickX, mStickY, mPaintBackground.getStrokeWidth(), mPaintForeground);
         super.onDraw(canvas); // debug mode
+
+        canvas.drawCircle(mDebugStickX, mDebugStickY, mPaintBackground.getStrokeWidth(), mPaintDebug);
+
+        canvas.drawCircle(mMarStartX, mMarStartY, 5, mPaintDebug);
+        canvas.drawLine(mMarStartX-mRadius,mMarStartY,mMarStartX+mRadius,mMarStartY,mPaintDebug);
+        canvas.drawLine(mMarStartX,mMarStartY-mRadius,mMarStartX,mMarStartY+mRadius,mPaintDebug);
+
+        canvas.drawText("ANG: " + mAngle, mStickX-mStickBound,mStickY-mStickBound-mPaintDebug.getTextSize()*4, mPaintDebug);
     }
 
     @Override
     public void onLayout(int width, int height, float startX, float startY, float endX, float endY) {
         super.onLayout(width, height, startX, startY, endX, endY);
         mRadius = (float) Math.hypot(mMarEndX-mMarStartX, mMarEndY-mMarStartY);
-        mStickX = mMarStartX - mRadius;
+        mStickX = mMarStartX + mRadius;
+        
     }
 
     @Override
     void onPlace(float x, float y) {
         // Shit-code below (mStickX, mStickY, mProgress)
-        mProgress = Math.abs((x - (mMarStartX-mRadius)) / (mRadius+ mRadius));
+
+        float localX = x - mMarStartX;
+        float localY = y - mMarStartY;
+
+        mAngle = (float) Math.toDegrees(Math.atan2(localY,localX));
+
+        if (mAngle < 0) {
+            mAngle = 360 + mAngle;
+        }
+
+        mProgress = mAngle / 360;
+
+        float sin = (float) Math.sin(mAngle * 0.015625f);
+        float cos = (float) Math.cos(mAngle * 0.015625f);
+
+        mDebugStickX = (float) (mMarStartX + mRadius*cos);
+        mDebugStickY = (float) (mMarStartY + mRadius*sin);
+
+        Log.d(TAG, "onPlace: SIN: " + sin + " COS: " + cos);
+
         mStickX = x;
-        Log.d(TAG, "onPlace: X: " + x + " START X: " + mMarStartX + " RADIUS: " + mRadius + " PROGRESS: " + mProgress);
+        mStickY = y;
     }
 }
